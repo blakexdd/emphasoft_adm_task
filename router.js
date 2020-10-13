3a7e787fe38a6421d7159f25fb015c9ff4039477
blakexdd <blakexxxd@gmail.com>
2020-10-13 16:26:45 +0300

    cb url changed

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ passport.use(new VKontakteStrategy(
     {
         clientID:     "7626527",
         clientSecret: "OAjUQOdBARIACLpw3iYz",
-        callbackURL:  `/auth/vkontakte/callback`,
+        callbackURL:  `https://admissiontask.herokuapp.com/auth/vkontakte/callback`,
         scope: ['status', 'email', 'friends', 'photo']
     },
     (req, accessToken, refreshToken, params, profile, done) => {
