016e1de23b6a782b19a2263d3b7dd6dfce9dcaa3
blakexdd <blakexxxd@gmail.com>
2020-10-13 17:54:42 +0300

    path c

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ passport.use(new VKontakteStrategy(
     {
         clientID:     "7626527",
         clientSecret: "OAjUQOdBARIACLpw3iYz",
-        callbackURL:  `http://178.252.108.45:${process.env.PORT}/auth/vkontakte/callback`,
+        callbackURL:  `/auth/vkontakte/callback`,
         scope: ['status', 'email', 'friends', 'photo']
     },
     (req, accessToken, refreshToken, params, profile, done) => {
