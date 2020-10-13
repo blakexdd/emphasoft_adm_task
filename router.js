c108b328c930d731ee66c2bc711b7938cbae9a5d
blakexdd <blakexxxd@gmail.com>
2020-10-13 16:24:55 +0300

    login path changed

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ passport.use(new VKontakteStrategy(
     {
         clientID:     "7626527",
         clientSecret: "OAjUQOdBARIACLpw3iYz",
-        callbackURL:  `http://${config.host}:${config.port}/auth/vkontakte/callback`,
+        callbackURL:  `/auth/vkontakte/callback`,
         scope: ['status', 'email', 'friends', 'photo']
     },
     (req, accessToken, refreshToken, params, profile, done) => {
