d859586f7d3b71f3217896d9271494f9b4454841
blakexdd <blakexxxd@gmail.com>
2020-10-13 16:52:13 +0300

    path c

--- router.js
+++ router.js
@@ -9,8 +9,7 @@ passport.use(new VKontakteStrategy(
     {
         clientID:     "7626527",
         clientSecret: "OAjUQOdBARIACLpw3iYz",
-        callbackURL:  "/auth/vkontakte/callback",
-        proxy: true,
+        callbackURL:  "http://admissiontask.herokuapp.com/auth/vkontakte/callback",
         scope: ['status', 'email', 'friends', 'photo']
     },
     (req, accessToken, refreshToken, params, profile, done) => {
